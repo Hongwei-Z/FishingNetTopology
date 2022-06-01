62d4e119ff227642dc5726320616701a7ac9167d
Hongwei Zhang <hn507820@dal.ca>
2022-06-01 16:10:59 -0300

    packet class update, generate sample data.

--- DevVersion/FNT.py
+++ DevVersion/FNT.py
@@ -13,6 +13,7 @@ class Dataset(object):
         self.data = data
 
 
+# Packet: Packet ID, Timestamp, Auditee, Auditor, Sensor Info, Dataset.
 class Packet(Sensor, Dataset):
 
     def __init__(self, pid, timestamp, auditee1, auditee2, sid, status, location, datatype, data):
@@ -27,11 +28,46 @@ class Packet(Sensor, Dataset):
         self.auditor = []
         self.disable = False
 
+    def get_pid(self):
+        return self.pid
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_auditee(self):
+        a1 = self.auditee1
+        a2 = self.auditee2
+
+        if a1 is None and a2 is None:
+            return [None, None]
+        elif a1 is not None and a2 is None:
+            return [a1.get_pid(), None]
+        elif a2 is not None and a1 is None:
+            return [None, a2.get_pid()]
+        else:
+            return [a1.get_pid(), a2.get_pid()]
+
+    def get_sid(self):
+        return self.sid
+
+    def get_status(self):
+        return self.status
+
+    def get_location(self):
+        return self.location
+
+    def get_datatype(self):
+        return self.datatype
+
+    def get_data(self):
+        return self.data
+
+    def print_sensor(self):
+        print("Sensor ID: {0}, Status: {1}, Location: {2}".format(self.sid, self.status, self.location))
+
+    def print_dataset(self):
+        print("Data: {0} = {1}".format(self.datatype, self.data))
+
     def print_packet(self):
-        print("Packet: ")
-        print("PID: {0}, Time: {1}, Auditee: {2}, {3}".format(self.pid, self.timestamp, self.auditee1, self.auditee2))
-        print("SID: {0}, Status: {1}, Location: {2}".format(self.sid, self.status, self.location))
-        print("Data Type: {0}, Data: {1}".format(self.datatype, self.data))
-
-
-
+        print("Packet ID: {0}, Timestamp: {1}, Auditee: [{2}, {3}]"
+              .format(self.pid, self.timestamp, self.auditee1, self.auditee2))
--- DevVersion/Test.py
+++ DevVersion/Test.py
@@ -3,5 +3,21 @@ import FNT
 s = FNT.Sensor("S1", "01", "Halifax")
 d = FNT.Dataset("Temperature", "18")
 
-packet = FNT.Packet(0, 1653510171, "0", "0", s.sid, s.status, s.location, d.datatype, d.data)
+a1 = FNT.Packet(1, 1653510171, 0, 0, s.sid, s.status, s.location, d.datatype, d.data)
+a2 = FNT.Packet(2, 1653510171, 0, 0, s.sid, s.status, s.location, d.datatype, d.data)
+
+packet = FNT.Packet(0, 1653510171, a1, a2, s.sid, s.status, s.location, d.datatype, d.data)
+
+packet.print_sensor()
+packet.print_dataset()
 packet.print_packet()
+
+# pid, timestamp, auditee1, auditee2, sid, status, location, datatype, data
+print(packet.get_pid())
+print(packet.get_timestamp())
+print(packet.get_auditee())
+print(packet.get_sid())
+print(packet.get_status())
+print(packet.get_location())
+print(packet.get_datatype())
+print(packet.get_data())
