2d8a13ae640937a44e28909fbe52f2c757f85c40
Hongwei Zhang <hn507820@dal.ca>
2022-06-21 17:11:04 -0300

    Complete the main methods

--- DevVersion/FNT.py
+++ DevVersion/FNT.py
@@ -1,7 +1,7 @@
 import math
 
 
-# Sensor: Sensor ID, Status, Location.
+# Sensor: SensorID, Status, Location.
 class Sensor(object):
 
     def __init__(self, sid, status, location):
@@ -18,7 +18,7 @@ class Dataset(object):
         self.data = data
 
 
-# Packet: Packet ID, Timestamp, Auditee, Auditor, Sensor Info, Dataset.
+# Packet: PacketID, Timestamp, Auditee, Auditor, Sensor Info, Dataset.
 class Packet(Sensor, Dataset):
 
     def __init__(self, pid, timestamp, sid, status, location, datatype, data, auditee1, auditee2):
@@ -38,16 +38,24 @@ class Packet(Sensor, Dataset):
         return self.timestamp
 
     def get_auditee(self):
-        a1 = self.auditee1
-        a2 = self.auditee2
-        if a1 is None and a2 is None:
+        adt1 = self.auditee1
+        adt2 = self.auditee2
+        if adt1 is None and adt2 is None:
             return [None, None]
-        elif a1 is not None and a2 is None:
-            return [a1.get_pid(), None]
-        elif a2 is not None and a1 is None:
-            return [None, a2.get_pid()]
+        elif adt1 is not None and adt2 is None:
+            return [adt1.get_pid(), None]
+        elif adt2 is not None and adt1 is None:
+            return [None, adt2.get_pid()]
         else:
-            return [a1.get_pid(), a2.get_pid()]
+            return [adt1.get_pid(), adt2.get_pid()]
+
+    def get_auditor(self):
+        if len(self.auditor) == 0:
+            return [None, None]
+        if len(self.auditor) == 1:
+            return [self.auditor[0].get_pid(), None]
+        if len(self.auditor) == 2:
+            return [self.auditor[0].get_pid(), self.auditor[1].get_pid()]
 
     def get_sid(self):
         return self.sid
@@ -65,13 +73,21 @@ class Packet(Sensor, Dataset):
         return self.data
 
     def print_sensor(self):
-        print("Sensor ID: {0}, Status: {1}, Location: {2}".format(self.sid, self.status, self.location))
+        print("SensorID: {0}, Status: {1}, Location: {2}".format(self.sid, self.status, self.location))
 
     def print_dataset(self):
         print("Data: {0} = {1}".format(self.datatype, self.data))
 
     def print_packet(self):
-        print("Packet ID: {0}, Timestamp: {1}, Auditee: {2}".format(self.pid, self.timestamp, self.get_auditee()))
+        print("PacketID: {0}, Timestamp: {1}, Auditee: {2}".format(self.pid, self.timestamp, self.get_auditee()))
+
+    def print_detail(self):
+        print("PacketID: {0}, Timestamp: {1}, Auditee: {2}, Auditor: {3}."
+              .format(self.pid, self.timestamp, self.get_auditee(), self.get_auditor()),
+              "SensorID: {0}, Status: {1}, Location: {2}."
+              .format(self.sid, self.status, self.location),
+              "Data: {0} = {1}."
+              .format(self.datatype, self.data))
 
 
 class FishingNet(object):
@@ -82,18 +98,78 @@ class FishingNet(object):
         self.auditeeList = []
 
         self.group = self.rate * 2 - 1  # Each group in FNT contains (rate * 2 - 1) packets.
-        self.countInitial = math.comb(self.rate + 1, 2)  # Counting nodes in the initial network
+        self.countInitial = math.comb(self.rate + 1, 2)  # Counting packets in the initial network.
 
-        edges = []  # Special nodes on the edge of the initial network.
+        edges = []  # Special packets on the edge of the initial network.
         for n in range(1, self.rate):
             upper = int(n * (n + 1) / 2)
             lower = upper + n
             edges.append(upper)
             edges.append(lower)
-        self.bound = edges
+        self.edge = edges
+
+    def on_edge(self):  # Determine whether a packet is on the edge.
+        return (self.pid - self.countInitial + 1) % self.group in [0, self.rate]
+
+    def on_top_edge(self):  # Determine whether a packet is on the top edge.
+        if (self.pid - self.countInitial + 1) % self.group == self.rate:
+            return True
+        if (self.pid - self.countInitial + 1) % self.group == 0:
+            return False
 
     def new_packet(self, timestamp, sid, status, location, datatype, data):  # Create a new packet.
-
         if self.pid == 0:  # First packet #0.
             packet = Packet(self.pid, timestamp, sid, status, location, datatype, data, None, None)
 
+        elif 0 < self.pid < self.countInitial:  # Packets in the initial network.
+            if self.pid in self.edge:  # Packets at the initial network edge have only one auditee.
+                adt = self.auditee_initial()
+                packet = Packet(self.pid, timestamp, sid, status, location, datatype, data, adt, None)
+                adt.auditor.append(packet)
+
+            else:  # Other packets at the initial network have two auditee.
+                adt1 = self.auditee_initial()
+                adt2 = self.auditee_initial()
+                packet = Packet(self.pid, timestamp, sid, status, location, datatype, data, adt1, adt2)
+                adt1.auditor.append(packet)
+                adt2.auditor.append(packet)
+
+        else:  # Packets in the formal network.
+            adt1 = self.auditee_formal()[0]
+            adt2 = self.auditee_formal()[1]
+            packet = Packet(self.pid, timestamp, sid, status, location, datatype, data, adt1, adt2)
+            adt1.auditor.append(packet)
+            adt2.auditor.append(packet)
+
+        self.auditeeList.append([packet, 0])
+        self.packets.append(packet)
+        self.pid += 1
+
+    def auditee_initial(self):  # Auditee for the initial network.
+        adt = self.auditeeList[0][0]
+        self.auditeeList[0][1] += 1
+
+        if self.auditeeList[0][1] >= 2:  # If a packet is approved twice, remove it from the list.
+            self.auditeeList.pop(0)
+
+        if self.countInitial < self.pid and self.on_edge():  # Last layer of the initial network.
+            adt = (self.packets[self.pid - self.group])
+            self.auditeeList[0][1] += 1
+        return adt
+
+    def auditee_formal(self):  # Auditee for the formal network.
+        if self.on_edge():  # Auditee for packets on the edge.
+            if self.on_top_edge():  # On top edge.
+                return [self.auditee_initial(), self.packets[self.pid - self.rate + 1]]
+            else:  # On bottom edge.
+                return [self.auditee_initial(), self.packets[self.pid - self.rate]]
+
+        else:  # Auditee for other packets.
+            return [self.packets[self.pid - self.rate], self.packets[self.pid - self.rate + 1]]
+
+    def display_fnt(self):  # Print all nodes
+        for i in range(len(self.packets)):
+            if self.packets[i].disable is True:
+                print("Packet {0} disabled.".format(i))
+            else:
+                self.packets[i].print_detail()
--- DevVersion/Test.py
+++ DevVersion/Test.py
@@ -1,23 +1,10 @@
 import FNT
+from datetime import datetime
 
-s = FNT.Sensor("S1", "00", "Halifax")
-d = FNT.Dataset("Temperature", "18")
+fnt = FNT.FishingNet(10)
+# timestamp, sid, status, location, datatype, data
+for i in range(150):
+    fnt.new_packet(datetime.now().strftime("%x"), "S1", "On", "HFX", "Tem", 18)
 
-a1 = FNT.Packet(1, 1653510171, s.sid, s.status, s.location, d.datatype, d.data, 0, 0)
-a2 = FNT.Packet(2, 1653510171, s.sid, s.status, s.location, d.datatype, d.data, 0, 0)
-
-packet = FNT.Packet(0, 1653510171, s.sid, s.status, s.location, d.datatype, d.data, a1, a2)
-
-packet.print_sensor()
-packet.print_dataset()
-packet.print_packet()
-
+fnt.display_fnt()
 # pid, timestamp, sid, status, location, datatype, data, auditee1, auditee2
-# print(packet.get_pid())
-# print(packet.get_timestamp())
-# print(packet.get_auditee())
-# print(packet.get_sid())
-# print(packet.get_status())
-# print(packet.get_location())
-# print(packet.get_datatype())
-# print(packet.get_data())
